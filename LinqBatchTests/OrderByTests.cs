4b6b35e3e117c75c4237281c52718ff42e4705e4
Jan Markvart <HonzaMark@seznam.cz>
2024-07-17 23:34:09 +0200

    expanded orderby tests

--- LinqBatchTests/OrderByTests.cs
+++ LinqBatchTests/OrderByTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using LinqBatch;
 
 namespace LinqBatchTests
@@ -6,7 +7,9 @@ namespace LinqBatchTests
     public class OrderByTests
     {
         int[][] intBatches;
+        List<List<int>> listIntBatches;
         string[][] stringBatches;
+        List<List<string>> listStringBatches;
         int[][] emptyBatches;
         string[][] emptyTopArray;
         [SetUp]
@@ -15,9 +18,15 @@ namespace LinqBatchTests
             int[] a = [10, 1, 3];
             int[] b = [-2, 5, -6];
             intBatches = [a, b];
+            List<int> lia = new(a);
+            List<int> lib = new(b);
+            listIntBatches = [lia, lib];
             string[] stringa = ["apple", "orange", "pear"];
             string[] stringb = ["car", "bike", "train"];
             stringBatches = [stringa, stringb];
+            List<string> lsa = new(stringa);
+            List<string> lsb = new(stringb);
+            listStringBatches = [lsa, lsb];
             int[] oa = [];
             int[] ob = [];
             emptyBatches = [oa, ob];
@@ -30,6 +39,8 @@ namespace LinqBatchTests
             int[][] expected = [[1, 3, 10], [-6, -2, 5]];
             var res = intBatches.OrderBatchesBy(x => x);
             Assert.That(res, Is.EqualTo(expected));
+            var res2 = listIntBatches.OrderBatchesBy(x => x);
+            Assert.That(res2, Is.EqualTo(expected));
         }
 
         [Test]
@@ -39,6 +50,13 @@ namespace LinqBatchTests
             var res = stringBatches.OrderBatchesBy(x => x);
             int i = 0;
             foreach (var s in res)
+            {
+                Assert.That(s, Is.EqualTo(expected[i]));
+                i++;
+            }
+            var res2 = listStringBatches.OrderBatchesBy(x => x);
+            i = 0;
+            foreach (var s in res2)
             {
                 Assert.That(s, Is.EqualTo(expected[i]));
                 i++;
