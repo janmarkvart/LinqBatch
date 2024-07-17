1a54aa866940dc3e883c1089bcb80770db4c3cd2
Jan Markvart <HonzaMark@seznam.cz>
2024-07-17 19:58:32 +0200

    expanded select and where tests

--- LinqBatchTests/SelectTests.cs
+++ LinqBatchTests/SelectTests.cs
@@ -9,6 +9,7 @@ namespace LinqBatchTests
         int[][] intBatches;
         List<List<int>> listIntBatches;
         string[][] stringBatches;
+        List<List<string>> listStringBatches;
         object[][] emptyBatches;
         object[][] emptyTopArray;
         [SetUp]
@@ -23,6 +24,9 @@ namespace LinqBatchTests
             string[] stringa = ["apple", "orange", "pear"];
             string[] stringb = ["car", "bike", "train"];
             stringBatches = [stringa, stringb];
+            List<string> lsa = new(stringa);
+            List<string> lsb = new(stringb);
+            listStringBatches = [lsa, lsb];
             object[] oa = [];
             object[] ob = [];
             emptyBatches = [oa, ob];
@@ -46,6 +50,8 @@ namespace LinqBatchTests
             int[][] expected = [newa, newb];
             var res = from x in intBatches select x * 2;
             Assert.That(res, Is.EqualTo(expected));
+            var res2 = from x in listIntBatches select x * 2;
+            Assert.That(res, Is.EqualTo(expected));
         }
 
         [Test]
@@ -53,6 +59,8 @@ namespace LinqBatchTests
         {
             var res = from x in stringBatches select x;
             Assert.That(res, Is.EqualTo(stringBatches));
+            var res2 = from x in listStringBatches select x;
+            Assert.That(res2, Is.EqualTo(stringBatches));
         }
 
         [Test]
--- LinqBatchTests/WhereTests.cs
+++ LinqBatchTests/WhereTests.cs
@@ -1,4 +1,4 @@
-﻿using System.Globalization;
+﻿using System.Collections.Generic;
 using System.Linq;
 using LinqBatch;
 
@@ -7,7 +7,9 @@ namespace LinqBatchTests
     public class WhereTests
     {
         int[][] intBatches;
+        List<List<int>> listIntBatches;
         string[][] stringBatches;
+        List<List<string>> listStringBatches;
         int[][] emptyBatches;
         string[][] emptyTopArray;
         [SetUp]
@@ -16,9 +18,15 @@ namespace LinqBatchTests
             int[] a = [1, 2, 3];
             int[] b = [4, 5, 6];
             intBatches = [a, b];
+            List<int> lia = new(a);
+            List<int> lib = new(b);
+            listIntBatches = [lia, lib];
             string[] stringa = ["apple", "orange", "pear"];
             string[] stringb = ["car", "bike", "train"];
             stringBatches = [stringa, stringb];
+            List<string> lsa = new(stringa);
+            List<string> lsb = new(stringb);
+            listStringBatches = [lsa, lsb];
             int[] oa = [];
             int[] ob = [];
             emptyBatches = [oa, ob];
@@ -30,8 +38,10 @@ namespace LinqBatchTests
         {
             //select only even numbers
             int[][] expected = [[2], [4, 6]];
-            var res = from x in intBatches where x%2==0 select x;
+            var res = from x in intBatches where x % 2 == 0 select x;
             Assert.That(res, Is.EqualTo(expected));
+            var res2 = from x in listIntBatches where x % 2 == 0 select x;
+            Assert.That(res2, Is.EqualTo(expected));
         }
 
         [Test]
@@ -39,8 +49,10 @@ namespace LinqBatchTests
         {
             //select numbers bigger or equal 3
             int[][] expected = [[3], [4, 5, 6]];
-            var res = from x in intBatches where x>=3  select x;
+            var res = from x in intBatches where x >= 3  select x;
             Assert.That(res, Is.EqualTo(expected));
+            var res2 = from x in listIntBatches where x >= 3 select x;
+            Assert.That(res2, Is.EqualTo(expected));
         }
 
         [Test]
@@ -48,8 +60,10 @@ namespace LinqBatchTests
         {
             //select only odd numbers bigger or equal 2
             int[][] expected = [[3], [5]];
-            var res = from x in intBatches where x%2==1 where x >= 2 select x;
+            var res = from x in intBatches where x % 2 == 1 where x >= 2 select x;
             Assert.That(res, Is.EqualTo(expected));
+            var res2 = from x in listIntBatches where x % 2 == 1 where x >= 2 select x;
+            Assert.That(res2, Is.EqualTo(expected));
         }
 
         [Test]
@@ -60,6 +74,13 @@ namespace LinqBatchTests
             var res = from x in stringBatches where x.Length > 4 select x;
             int i = 0;
             foreach (string[] s in res)
+            {
+                Assert.That(s, Is.EqualTo(expected[i]));
+                i++;
+            }
+            var res2 = from x in listStringBatches where x.Length > 4 select x;
+            i = 0;
+            foreach (string[] s in res2)
             {
                 Assert.That(s, Is.EqualTo(expected[i]));
                 i++;
